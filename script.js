7e9654f8553ca272bc80ce8498915118942d5c3a
chiranjeevi gurrapu <117752857+chiranjeevi3398@users.noreply.github.com>
2023-07-03 23:48:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function updateElementText() {
+  return new Promise((resolve) => {
+    setTimeout(() = { 
+      resolve("Hello, world!");
+}, 1000);
+});
+}
+
+ // Update HTML element text
+
+const outputElement = document.getElemen
+
+tById("output"); oupdateElementText().then((text) => {
+  outputElement.textContent = text; });
